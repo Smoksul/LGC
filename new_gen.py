5704320ba528cd4c01b7b475ac51bba085f52450
glidercollider <40713612+glidercollider@users.noreply.github.com>
2018-06-30 20:42:00 +0200

    Update new_gen.py

--- new_gen.py
+++ new_gen.py
@@ -1,5 +1,5 @@
 def gen(a, b, c, rule): #bierze sasiadow i numer reguly w postaci DZIESIETNEJ od 0 do 255
-	tabv = [0,1,2,3,4,5,6,7]
+	tabv = [0,1,2,3,4,5,6,7] #tu sa loswe liczby, zaraz i tak beda nadpisane
 	for i in range (0,8):
 		tempr=int(rule)
 		rule=int(rule)//2
