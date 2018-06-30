7e53e598c9b7b69ee5f8c7fff0e1d9b93ed08669
k3mik <wmikke98@gmail.com>
2018-06-30 22:32:04 +0200

    qwert

--- cut.py
+++ cut.py
@@ -1,9 +1,13 @@
 def cut(rlchoice, linechoice):
     fname="../out/out_"+str(rlchoice)+".dat"
     file=open(fname)
-    line=file.readline(linechoice)
     refline=""
-    for iter in range(0, len(line)):
-        if line[iter]!=' ':
-            refline=refline+line[iter]
+    iter=0
+    while iter!=linechoice:
+        file.readline()
+        iter+=1
+    line=file.readline()
+    for iter1 in range(0, len(line)-1):
+        if line[iter1]!=' ':
+            refline=refline+line[iter1]
     return refline
