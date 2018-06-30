5623e7933c970e13f1efd1273a977d90d8b11683
k3mik <wmikke98@gmail.com>
2018-06-30 18:09:45 +0200

    update

--- generator_linii.py
+++ generator_linii.py
@@ -42,14 +42,14 @@ def gen(a, b, c, rule): #funkcja generujaca wynik na podstawie sasiadow
 			else:
 				return int(rule[7])
 
-
-A = numpy.array([[1, 0, 1, 0, 0, 0, 1, 0, 1, 1]]) #jakas przykladowa macierz MxN
-M=30 #liczba wierszy ktore beda PO wykonaniu petli
+M=10 #liczba wierszy ktore beda PO wykonaniu petli
 N=10 #liczba kolumn
+A = numpy.zeros((M,N), dtype='i') #jakas przykladowa macierz MxN
 
 rule=rules()
+A[0][3] = 1
+print A
 for i in range(0, M-1): #wlasciwa petla dopisujaca kolejny wiersz
-		A.resize((i+2, N))
 		for j in range(0, N): # i numeruje wiersze, j kolumny
 			if(j == 0):
 				A[i+1][j] = A[i][j]
@@ -57,6 +57,8 @@ for i in range(0, M-1): #wlasciwa petla dopisujaca kolejny wiersz
 				A[i+1][j] = A[i][j]
 			else:
 				A[i+1][j] = gen(A[i][j-1], A[i][j], A[i][j+1], rule)
-
-plt.matshow(A, cmap=plt.cm.gray)
+print ("")
+print A
+plt.matshow(A)
+#plt.imshow(A)
 plt.show()
