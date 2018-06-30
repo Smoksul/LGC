8dc1d65881f86d335137a430b07d75c63e99de9f
glidercollider <40713612+glidercollider@users.noreply.github.com>
2018-06-30 15:45:17 +0200

    Update generator_linii.py

--- generator_linii.py
+++ generator_linii.py
@@ -11,8 +11,8 @@ N=5 #liczba kolumn
 for i in range(0, M-1): #właściwa pętla dopisująca kolejny wiersz
 		for j in range(0, N): # i numeruje wiersze, j kolumny
 			if(j == 0):
-				A[i+1][j] = gen(0, A[i][j], A[i][j+1])
+				A[i+1][j] = A[i][j]
 			elif(j == N-1):
-				A[i+1][j] = gen(A[i][j-1], A[i][j], 0)
+				A[i+1][j] = A[i][j]
 			else:
 				A[i+1][j] = gen(A[i][j-1], A[i][j], A[i][j+1])
