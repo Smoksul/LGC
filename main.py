f001d7ffbc73334f047d9aee81eee6f8e3bdccfc
Smoksul <40713406+Smoksul@users.noreply.github.com>
2018-06-30 22:00:37 +0200

    123

--- main.py
+++ main.py
@@ -9,17 +9,13 @@ import rulesmain as rul
 import plotmain as plm
 import cut
 
-#M=512#liczba wierszy
-#N=256 #liczba kolumn
-#rlnbr=input("Podaj numer zasady.")
-#A = numpy.zeros((M,N), dtype='i') #macierz MxN inicjowana zerami
-#rule=rul.rules(rlnbr)
-#rd.randgen(A,N)
-print(cut.cut(111, 350))
+M=512#liczba wierszy
+N=256 #liczba kolumn
+A = numpy.zeros((M,N), dtype='i') #macierz MxN inicjowana zerami
+rd.setgen(A, N, cut.cut(111, 350))
 
-#for rlnbr in range(1,128):
-#    rule=rul.rules(rlnbr)
-#    cell.evolve(A, N, M, rule)
-#    plm.plot(A,rlnbr)
-
+for rlnbr in range(1,128):
+    rule=rul.rules(rlnbr)
+    cell.evolve(A, N, M, rule)
+    plm.plot(A,rlnbr)
 
