6761f1fa074c3635fadc728a1125c6948989f762
Smoksul <40713406+Smoksul@users.noreply.github.com>
2018-06-30 21:56:58 +0200

    123

--- cut.py
+++ cut.py
@@ -1,4 +1,9 @@
 def cut(rlchoice, linechoice):
     fname="../out/out_"+str(rlchoice)+".dat"
     file=open(fname)
-    return file.readline(linechoice)
+    line=file.readline(linechoice)
+    refline=""
+    for iter in range(0, len(line)):
+        if line[iter]!=' ':
+            refline=refline+line[iter]
+    return refline
