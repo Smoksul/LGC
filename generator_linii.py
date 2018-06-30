ce74eee67733c828ad07272495502897c43618d3
glidercollider <40713612+glidercollider@users.noreply.github.com>
2018-06-30 16:02:25 +0200

    Update generator_linii.py

--- generator_linii.py
+++ generator_linii.py
@@ -4,11 +4,12 @@ def gen(a, b, c): #funkcja generujaca wynik na podstawie sasiadow
 	return b
 
 A = numpy.array([[1, 0, 1, 0, 0], [0, 0, 0, 0, 1], [0, 0, 2, 0, 0], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]) #jakas przykladowa macierz MxN
-M=6 #liczba wierszy
+M=6 #liczba wierszy ktore beda PO wykonaniu petli
 N=5 #liczba kolumn
 
 
 for i in range(0, M-1): #wlasciwa petla dopisujaca kolejny wiersz
+		A.resize((i+2, N))
 		for j in range(0, N): # i numeruje wiersze, j kolumny
 			if(j == 0):
 				A[i+1][j] = A[i][j]
