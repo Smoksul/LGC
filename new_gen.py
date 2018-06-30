6f42b03b0d5f17d266ac080e0b929bd46c6a7924
glidercollider <40713612+glidercollider@users.noreply.github.com>
2018-06-30 20:26:49 +0200

    Update new_gen.py

--- new_gen.py
+++ new_gen.py
@@ -1,4 +1,4 @@
-def gen(a, b, c, rule):
+def gen(a, b, c, rule): #bierze sasiadow i numer reguly w postaci DZIESIETNEJ od 0 do 255
 	tabv = [0,1,2,3,4,5,6,7]
 	for i in range (0,8):
 		tempr=int(rulenbr)
