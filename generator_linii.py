fb1b858c0b1d3669e08e248ac6adc9ff24d8f5f0
glidercollider <40713612+glidercollider@users.noreply.github.com>
2018-06-30 15:46:43 +0200

    Update generator_linii.py

--- generator_linii.py
+++ generator_linii.py
@@ -1,14 +1,14 @@
 import numpy
 
-def gen(a, b, c): #funkcja generująca wynik na podstawie sąsiadów
+def gen(a, b, c): #funkcja generujaca wynik na podstawie sasiadow
 	return b
 
-A = numpy.array([[1, 0, 1, 0, 0], [0, 0, 0, 0, 1], [0, 0, 2, 0, 0], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]) #jakaś przykładowa macierz MxN
+A = numpy.array([[1, 0, 1, 0, 0], [0, 0, 0, 0, 1], [0, 0, 2, 0, 0], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]) #jakas przykladowa macierz MxN
 M=6 #liczba wierszy
 N=5 #liczba kolumn
 
 
-for i in range(0, M-1): #właściwa pętla dopisująca kolejny wiersz
+for i in range(0, M-1): #wlasciwa petla dopisujaca kolejny wiersz
 		for j in range(0, N): # i numeruje wiersze, j kolumny
 			if(j == 0):
 				A[i+1][j] = A[i][j]
