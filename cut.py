04d3ec5dfa8eec90cde92e6f8e7ef2332035a9ec
Smoksul <40713406+Smoksul@users.noreply.github.com>
2018-06-30 21:45:40 +0200

    123

--- cut.py
+++ cut.py
@@ -1,4 +1,4 @@
-def cutline(rlchoice, linechoice):
+def cut(rlchoice, linechoice):
     fname="./out/out_"+str(rlchoice)+".dat"
     file=open(fname)
     return file.readline(linechoice)
--- main.py
+++ main.py
@@ -9,16 +9,17 @@ import rulesmain as rul
 import plotmain as plm
 import cut
 
-M=512#liczba wierszy
-N=256 #liczba kolumn
+#M=512#liczba wierszy
+#N=256 #liczba kolumn
 #rlnbr=input("Podaj numer zasady.")
-A = numpy.zeros((M,N), dtype='i') #macierz MxN inicjowana zerami
+#A = numpy.zeros((M,N), dtype='i') #macierz MxN inicjowana zerami
 #rule=rul.rules(rlnbr)
-rd.randgen(A,N)
+#rd.randgen(A,N)
+print(cut.cut(111, 350))
 
-for rlnbr in range(1,128):
-    rule=rul.rules(rlnbr)
-    cell.evolve(A, N, M, rule)
-    plm.plot(A,rlnbr)
-    print(cut.cut(A, 111, 350))
+#for rlnbr in range(1,128):
+#    rule=rul.rules(rlnbr)
+#    cell.evolve(A, N, M, rule)
+#    plm.plot(A,rlnbr)
+
 
