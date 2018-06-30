dcc2303b88c2aacb523afa3dc942ba2e0f67cc97
Smoksul <40713406+Smoksul@users.noreply.github.com>
2018-06-30 20:36:02 +0200

    delete

--- new_gen.py
+++
@@ -1,7 +0,0 @@
-def gen(a, b, c, rule): #bierze sasiadow i numer reguly w postaci DZIESIETNEJ od 0 do 255
-	tabv = [0,1,2,3,4,5,6,7]
-	for i in range (0,8):
-		tempr=int(rule)
-		rule=int(rule)//2
-		tabv[i]=tempr-rule*2
-	return tabv[4*c+2*b+a]
